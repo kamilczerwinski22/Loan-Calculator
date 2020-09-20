584bfaee5d45052496688527991596bea2bfaac8
Kamil Czerwinski <kamilcze31@gmail.com>
2020-09-20 19:11:34 +0200

    Stage 1/4, OOP version

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -1,9 +1,3 @@
-loan_principal = 'Loan principal: 1000'
-final_output = 'The loan has been repaid!'
-first_month = 'Month 1: repaid 250'
-second_month = 'Month 2: repaid 250'
-third_month = 'Month 3: repaid 500'
-
 class LoanCalculator:
     """
     A class representing Loan Calculator
